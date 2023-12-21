7e580f56692cc1aea477071dfaa5a74a99bf13c0
ktxiaok <ktxiaok@foxmail.com>
2023-12-21 15:21:39 +0800

    update build scripts

--- build_windows.py
+++ build_windows.py
@@ -2,8 +2,10 @@ import PyInstaller.__main__
 from send2trash import send2trash
 import shutil
 import os
+from zipfile import ZipFile
 
 OUTPUT_DIR_PATH = "./build/windows/DontTouchBlocks"
+OUTPUT_ZIP_FILE_PATH = OUTPUT_DIR_PATH + ".zip"
 
 if os.path.exists(OUTPUT_DIR_PATH):
     send2trash(OUTPUT_DIR_PATH)
@@ -21,5 +23,12 @@ PyInstaller.__main__.run((
 ))
 
 shutil.copytree("./resources", os.path.join(OUTPUT_DIR_PATH, "resources"))
+
+with ZipFile(OUTPUT_ZIP_FILE_PATH, "w") as zipobj:
+    for rootpath, dirnames, filenames in os.walk(OUTPUT_DIR_PATH):
+        for filename in filenames:
+            fullpath = os.path.join(rootpath, filename)
+            relpath = os.path.relpath(fullpath, OUTPUT_DIR_PATH)
+            zipobj.write(fullpath, relpath)
 
 print("Successful build!")
